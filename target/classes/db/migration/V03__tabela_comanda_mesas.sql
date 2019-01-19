ff915403cab8ce9b4f14908ea9871ca6a254d75a
Thiago Tarso <t.tarsoo@hotmail.com>
2019-01-19 11:26:14 +0100

    ajustes na estrutura.

--- target/classes/db/migration/V03__tabela_comanda_mesas.sql
+++
@@ -1,7 +0,0 @@
-create table comanda_mesa (
-   id_comanda BIGINT NOT NULL,
-   id_mesa BIGINT NOT NULL,
-   PRIMARY KEY(id_comanda, id_mesa),
-   FOREIGN KEY (id_comanda) REFERENCES comanda(comanda_id),
-   FOREIGN KEY (id_mesa) REFERENCES mesa(mesa_id)
-)
--- target/classes/db/migration/V05__Inserir_grupos.sql
+++
@@ -1,2 +0,0 @@
-insert into grupo(id, nome) values (1, 'Adminstrador');
-insert into grupo(id, nome) values (2, 'vendedor');
